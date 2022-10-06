dd64132a6b687101fe169119094e03954da7dd6a
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-06 14:13:30 +0100

    passed lesson

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    list = arguments[0]
+    for(i = 1; i < arguments.length; i++){
+        if (list.includes(arguments[i])){
+            for(j = 0; j < list.length; j++){
+                if(list[j] == arguments[i]){
+                    list.splice(j, 1)
+                }
+            }
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
