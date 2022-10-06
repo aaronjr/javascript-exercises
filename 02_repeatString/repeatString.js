ee9b516a2f03628fc98922b784e090bf8779ff87
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-06 13:11:46 +0100

    passed lesson

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(input, num) {
 
+    if(num < 0){
+        return "ERROR"
+    }
+    else{
+        let word = ""
+        for(i = 0; i < num; i++){
+            word += input
+        }
+        return word;
+    }
 };
 
 // Do not edit below this line
