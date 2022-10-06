33d189fd363dd063afa6db24f9a7dfbd46ffb617
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-06 13:17:50 +0100

    passed lesson

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    word = ""
+    for(i = input.length - 1; i >= 0 ; i-- ){
+        word += input[i]
+    }
+    return word
 };
 
 // Do not edit below this line
