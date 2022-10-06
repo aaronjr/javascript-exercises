d53c7a551df0df5f378196efa68c27f1abb278de
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-06 15:26:27 +0100

    passed lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  c = Math.round(((temp - 32) * (5/9)) * 10) / 10
+  return c
 };
 
-const ctof = function() {
 
+const ctof = function(temp) {
+ f = Math.round((temp * 9/5 + 32) * 10) / 10
+ return f
 };
+
 
 // Do not edit below this line
 module.exports = {
