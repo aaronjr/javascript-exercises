9e4c35f10d9062bf0f1d5a5250176167f4e0488e
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-07 17:14:57 +0100

    passed lessons

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  total = 0
+  if(array.length == 0){
+    return total
+  }
+  else{
+    for(i in array){
+      total += parseInt(array[i])
+    }
+  }
+  return total
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  total = array[0]
+  array.shift()
+  for(i in array){
+    total *= parseInt(array[i])
+  }
+  return total
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a * a
+	for(let i = 1; i < b - 1; i++){
+    total *= a 
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0){
+    return 1;
+  }
+  let total = 1;
+  for (let i = n; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
