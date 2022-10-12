84359fb52338284574755dabd5eac8137ea91529
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-12 11:35:37 +0100

    pass lesson

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+
+    let previous = 0
+    let total = 1
+   
+    for (start = 1; start < num; start++){
+        let temp = total
+        total = total + previous
+        previous = temp
+    }
+    return total
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
