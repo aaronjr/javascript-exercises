73e50e08e20dc806eb6b03054b87e898f2dda82a
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-12 10:45:45 +0100

    passed lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    string = a.toLowerCase().replace(/[^a-z]/g, "");
+    
+    return (string.split("").reverse().join("") == string)
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// first word
+//aReplace = a.replace(',', " ")
+//aJoin = aReplace.split("").join("")
+// second word
+//reversed = aReplace.split("").reverse().join("")
+
+//return aJoin + " -- " + reversed
